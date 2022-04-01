7ffe14e0cc66189e4dcd8be8847edfbf61cf08be
imCrong <yoonsuk1101@gmail.com>
2022-04-01 20:24:38 +0900

    First Commit

---
+++ test.py
@@ -0,0 +1,11 @@
+jobs ={'김연아':'피겨스케이팅',
+       '정현':'테니스'}
+for key,value in jobs.items():
+    if(key == "정현"):
+        var =key
+        result = value
+        break
+print(var+result)
+a = jobs["정현"]
+print(a)
+
